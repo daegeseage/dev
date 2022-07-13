f693eeeb3723cc7bc50ff0c86e157d725d2f2bad
daegeseage <daegeseage98@gmail.com>
2022-07-13 11:30:49 +0300

    added realisation of simple log to script

--- clear_media.sh
+++ clear_media.sh
@@ -2,14 +2,17 @@
 
 # This script must delete older then 500 days media images. 
 
-# go do docker directory whith docker containers directory
-# count targets based on dircetoris whith docker container
-# NUM_OF_DIRS=$(find -type d | sed -e 's/\.\///g' | grep -v '\.' | tr '\n' ' ' | wc -w)
-# DIRS=$(find -type d | sed -e 's/\.\///g' | grep -v '\.' | tr '\n' ' ')
+
+# Set key for df command for more simple arithmetic operation whith bytes
+BLOCK_NUM_KEY="-B1"
+
+# Measure size before cleaning 
+BEFORE=$( df "$BLOCK_NUM_KEY" | grep '/dev/mapper/vg_sys-docker' | awk '{print $3}' )
 
 # count targets based on active docker containers
-NUM_OF_DIRS=$(sudo docker ps | grep gearman | tr ' ' '\n' | grep _gearman | awk -F '_' '{print $1}' | tr '\n' ' ' | wc -w)
-DIRS=$(sudo docker ps | grep gearman | tr ' ' '\n' | grep _gearman | awk -F '_' '{print $1}' | tr '\n' ' ')
+NUM_OF_DIRS=$(docker ps | grep gearman | tr ' ' '\n' | grep _gearman | awk -F '_' '{print $1}' | tr '\n' ' ' | wc -w)
+DIRS=$(docker ps | grep gearman | tr ' ' '\n' | grep _gearman | awk -F '_' '{print $1}' | tr '\n' ' ')
+CONTAINER_BEFORE=$DIRS
 COUNTER=0
 while [ "$COUNTER" -lt "$NUM_OF_DIRS" ]
 do
@@ -25,6 +28,16 @@ do
     DIRS=$(echo "$DIRS" | sed -e 's/'$CLIENT' //g')
     COUNTER=$((++COUNTER))
 done
-    
 
+# Count docker container upped after clearing
+CONTAINER_AFTER=$(docker ps | grep gearman | tr ' ' '\n' | grep _gearman | awk -F '_' '{print $1}' | tr '\n' ' ')
 
+# Measure size after cleaning 
+AFTER=$( df "$BLOCK_NUM_KEY" | grep '/dev/mapper/vg_sys-docker' | awk '{print $3}' )
+
+let CLEANED="BEFORE - AFTER"
+DATE=$(date)
+WAS_CLEANED=$(echo -n "was cleaned:  "; numfmt --to=iec "$CLEANED")
+cd /docker
+echo -e "------------\n""containers before:$CONTAINER_BEFORE\ncontainers after:$CONTAINER_AFTER\n""$WAS_CLEANED" on "$DATE" >> test_log
+cp test_log /home/nsazhnev
